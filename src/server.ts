758e31e0dc94b9fdc43188aacbf7e6889926f9b1
alanflp <alan.felipe@querodelivery.com>
2021-06-21 20:12:45 -0300

    feat: :sparkles: add message server
    Alteração de código

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Quero' })
+  return response.json({ message: 'Hello Quero Delivery' })
 })
 
 app.listen(3333);
